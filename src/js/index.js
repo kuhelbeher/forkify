8b6b2551f93e88713a28cbc200c7bdbd206a6252
kuhelbeher <kuhelbeher@users.noreply.github.com>
2018-06-11 12:01:32 +0300

    Implemented rendering search results

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,39 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-const search = new Search('pizza');
+/* Global stat of the app 
+	- search object
+	- current recipe object
+	- shopping list object
+	- liked recipes
+*/
+const state = {};
 
-console.log(search.getResults());
+const controlSearch = async () => {
+	// 1 get the query from the view
+	const query = searchView.getInput(); 
+
+	if (query) {
+		// 2 new search object and add to state
+
+		state.search = new Search(query);
+
+		// 3 prepare UI for results
+		searchView.clearInput();
+		searchView.clearResults();
+
+		// 4 search for recipes
+		await state.search.getResults();
+
+		// 5 render results on UI
+		searchView.renderResults(state.search.result);
+	}
+}
+
+elements.searchForm.addEventListener('submit', e => {
+	e.preventDefault();
+
+	controlSearch();
+});
+
